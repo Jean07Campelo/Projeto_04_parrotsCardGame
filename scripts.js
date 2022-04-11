7079b319666a42c0d487b012c6af8ee6339d6c3e
Jean Campelo <jean93campelo@gmail.com>
2022-04-11 09:06:17 -0300

    inserir quantidade cartas de acordo com entrada

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
-/*
+
 let quantCartas;
+let container = document.querySelector(".cartas");
+
 
 //lê entrada do usuario e converte para number
 function leituraEntrada () {
@@ -15,14 +17,24 @@ function verificaEntrada () {
         
     }
 //chamar funcao para começar jogo
+    iniciarJogo()
     console.log(`parabéns, entrada válida: ${quantCartas}`)
+    return quantCartas;    
 }
 
 
-//chamada de funcoes
-leituraEntrada()
+function iniciarJogo() {
+    for (let i = 0; i < quantCartas; i++) {
 
-*/
+        container.innerHTML += 
+        `
+        <li class="carta centralizar" onclick="selecionar(this)">
+            <img class="verso" src="/images/front.png">
+        </li>
+
+        `
+    }
+}
 
 function selecionar (elemento) {
     let verso = elemento.querySelector(".verso")
@@ -31,3 +43,20 @@ function selecionar (elemento) {
     verso.classList.toggle("esconder")
     frente.classList.toggle("esconder")
 }
+
+
+//chamada de funcoes
+leituraEntrada()
+
+/*
+//variaveis com as imagens
+let imagem1 = "/images/bobrossparrot.gif";
+let imagem2 = "/images/explodyparrot.gif";
+let imagem3 = "/images/fiestaparrot.gif";
+let imagem4 = "/images/metalparrot.gif";
+let imagem5 = "/images/revertitparrot.gif";
+let imagem6 = "/images/tripletsparrot.gif";
+let imagem7 = "/images/unicornparrot.gif";
+
+let bancoImagens = [imagem1, imagem2, imagem3, imagem4, imagem5, imagem6, imagem7];
+*/
