31041c13609cbba0ae68f40267b4c2515bd01f5e
Jean Campelo <jean93campelo@gmail.com>
2022-04-11 09:34:08 -0300

    funcao para embaralhar banco de imagens

--- scripts.js
+++ scripts.js
@@ -2,6 +2,22 @@
 let quantCartas;
 let container = document.querySelector(".cartas");
 
+//funcao usada na .sort
+function comparador() { 
+	return Math.random() - 0.5; 
+}
+
+
+//variaveis com as imagens
+let imagem1 = "/images/bobrossparrot.gif";
+let imagem2 = "/images/explodyparrot.gif";
+let imagem3 = "/images/fiestaparrot.gif";
+let imagem4 = "/images/metalparrot.gif";
+let imagem5 = "/images/revertitparrot.gif";
+let imagem6 = "/images/tripletsparrot.gif";
+let imagem7 = "/images/unicornparrot.gif";
+let bancoImagens = [imagem1, imagem2, imagem3, imagem4, imagem5, imagem6, imagem7];
+
 
 //lê entrada do usuario e converte para number
 function leituraEntrada () {
@@ -14,15 +30,22 @@ function verificaEntrada () {
     while (quantCartas % 2 !== 0 || quantCartas < 4 || quantCartas > 14) {
         console.log(`quantidade inválida: ${quantCartas}`)
         leituraEntrada()
-        
     }
 //chamar funcao para começar jogo
+    embaralharBanco()
     iniciarJogo()
     console.log(`parabéns, entrada válida: ${quantCartas}`)
     return quantCartas;    
 }
 
 
+//embaralhar banco de imagens
+function embaralharBanco () {
+    bancoImagens = bancoImagens.sort(comparador);
+    }
+
+
+//iniciar jogo com entrada usuario
 function iniciarJogo() {
     for (let i = 0; i < quantCartas; i++) {
 
@@ -47,16 +70,3 @@ function selecionar (elemento) {
 
 //chamada de funcoes
 leituraEntrada()
-
-/*
-//variaveis com as imagens
-let imagem1 = "/images/bobrossparrot.gif";
-let imagem2 = "/images/explodyparrot.gif";
-let imagem3 = "/images/fiestaparrot.gif";
-let imagem4 = "/images/metalparrot.gif";
-let imagem5 = "/images/revertitparrot.gif";
-let imagem6 = "/images/tripletsparrot.gif";
-let imagem7 = "/images/unicornparrot.gif";
-
-let bancoImagens = [imagem1, imagem2, imagem3, imagem4, imagem5, imagem6, imagem7];
-*/
